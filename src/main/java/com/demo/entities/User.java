ca951dea0f4440b7625fad2377b89002fbb1e015
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-07 20:14:22 -0300

    H2 database, test profile, JPA

--- src/main/java/com/demo/entities/User.java
+++ src/main/java/com/demo/entities/User.java
@@ -2,6 +2,11 @@ package com.demo.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,16 +18,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String nome;
+	private String name;
 	
 	private String email;
 	
-	private String fone;
+	private String phone;
 	
 	private String password;
 	
