1c9300cd88fe87bf269bbbb8692a949a34b3486a
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-07 18:33:02 -0300

    Project created

---
+++ src/main/java/com/demo/WebServicesSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
