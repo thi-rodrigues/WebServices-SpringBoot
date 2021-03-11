d58505ae26e83abc408b9f299bdf852c7bff63da
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-11 13:22:23 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/demo/config/TestConfig.java
+++ src/main/java/com/demo/config/TestConfig.java
@@ -48,6 +48,16 @@ public class TestConfig implements CommandLineRunner {
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 
 		User u1 = new User(null, "Thiago Rodrigues", "thiago@gmail.com", "10998877665", "123456");
 		User u2 = new User(null, "João Augusto", "joao@gmail.com", "56677889901", "654321");
