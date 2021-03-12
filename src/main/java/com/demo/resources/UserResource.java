ef8667fdde4e5e93980e69f5cb0c219c85e4d42e
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-12 03:55:29 -0300

    User insert, delete, update

--- src/main/java/com/demo/resources/UserResource.java
+++ src/main/java/com/demo/resources/UserResource.java
@@ -3,10 +3,16 @@ package com.demo.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.entities.User;
@@ -31,4 +37,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(findById);
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User save(@RequestBody User user){
+		return userService.save(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) {
+		userService.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public User update(@PathVariable Long id, @RequestBody User user) {
+		return userService.update(id, user);
+	}
 }
--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -19,8 +19,25 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
-	public User findById( Long id ) {
+	public User findById(Long id) {
 		Optional<User> findById = userRepository.findById(id);
 		return findById.get();
+	}
+	
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+	
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User user) {
+		User one = userRepository.getOne(id); // ID: 3
+		one.setName(user.getName());
+		one.setEmail(user.getEmail());
+		one.setPhone(user.getPhone());
+		
+		return userRepository.save(one);
 	}
 }
