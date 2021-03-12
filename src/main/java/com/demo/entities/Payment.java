c6e50d20f2ef5faf0548d06ff357c1e8e457aecf
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-12 00:43:11 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/demo/entities/Payment.java
@@ -0,0 +1,87 @@
+package com.demo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Instant momemnt;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {
+	}
+
+	public Payment(Long id, Instant momemnt, Order order) {
+		super();
+		this.id = id;
+		this.momemnt = momemnt;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomemnt() {
+		return momemnt;
+	}
+
+	public void setMomemnt(Instant momemnt) {
+		this.momemnt = momemnt;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
